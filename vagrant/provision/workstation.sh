83b80f652d452f0e4216a6150154e06923e2accf
James Leopold <jleopold@shadow-soft.com>
2017-10-04 16:56:59 -0400

    updated workstation provision script

--- vagrant/provision/workstation.sh
+++ vagrant/provision/workstation.sh
@@ -3,6 +3,7 @@ systemctl stop firewalld
 
 yum -y group install "Server With GUI"
 systemctl set-default graphical.target
+reboot
 
 yum -y install wget
 wget https://packages.chef.io/files/stable/chefdk/2.3.4/el/7/chefdk-2.3.4-1.el7.x86_64.rpm -P /tmp
