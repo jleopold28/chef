1a35983d31dfa6213687604c3371739260d2282a
James Leopold <jleopold@shadow-soft.com>
2017-10-05 11:03:29 -0400

    updating provision scripts

--- vagrant/provision/bootstrap.sh
+++ vagrant/provision/bootstrap.sh
@@ -2,30 +2,32 @@
 systemctl disable firewalld
 systemctl stop firewalld
 
-# Download and install Chef Server RPM
 yum -y install wget
-wget https://packages.chef.io/files/stable/chef-server/12.16.14/el/7/chef-server-core-12.16.14-1.el7.x86_64.rpm -P /tmp
-rpm -Uvh /tmp/chef-server-core-*.rpm
-
 chown -R vagrant:vagrant /home/vagrant
 mkdir /home/vagrant/certs
 
-# Start all services, Configure Server
-chef-server-ctl reconfigure
-chef-server-ctl user-create james James Leopold jleopold@shadow-soft.com password --filename /home/vagrant/certs/james.pem
-chef-server-ctl org-create cheflab "Chef Lab Org" --association_user james --filename /home/vagrant/certs/cheflab-validator.pem
+# Download and install Chef Server RPM
+if [ ! -f /tmp/chef-server-core*.rpm ]; then
+  wget https://packages.chef.io/files/stable/chef-server/12.16.14/el/7/chef-server-core-12.16.14-1.el7.x86_64.rpm -P /tmp
+  rpm -Uvh /tmp/chef-server-core-*.rpm
+  
+  # Start all services, Configure Server
+  chef-server-ctl reconfigure
+  chef-server-ctl user-create james James Leopold jleopold@shadow-soft.com password --filename /home/vagrant/certs/james.pem
+  chef-server-ctl org-create cheflab "Chef Lab Org" --association_user james --filename /home/vagrant/certs/cheflab-validator.pem
 
-# Installing Chef Manage
-chef-server-ctl install chef-manage
-chef-server-ctl reconfigure
-chef-manage-ctl reconfigure --accept-license
+  # Installing Chef Manage
+  chef-server-ctl install chef-manage
+  chef-server-ctl reconfigure
+  chef-manage-ctl reconfigure --accept-license
 
-# Install Reporting
-chef-server-ctl install opscode-reporting
-chef-server-ctl reconfigure
-opscode-reporting-ctl reconfigure --accept-license
+  # Install Reporting
+  chef-server-ctl install opscode-reporting
+  chef-server-ctl reconfigure
+  opscode-reporting-ctl reconfigure --accept-license
 
-chef-server-ctl install chef-manage
+  chef-server-ctl install chef-manage
+fi
 
 cat >> /etc/hosts <<EOL
 192.168.123.10 chef-server
--- vagrant/provision/workstation.sh
+++ vagrant/provision/workstation.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
+
 systemctl disable firewalld
 systemctl stop firewalld
 
-yum -y group install "Server With GUI"
-systemctl set-default graphical.target
-reboot
-
 yum -y install wget
-wget https://packages.chef.io/files/stable/chefdk/2.3.4/el/7/chefdk-2.3.4-1.el7.x86_64.rpm -P /tmp
-rpm -Uvh /tmp/chefdk*.rpm
+
+if [ ! -f /tmp/chefdk*.rpm ]; then
+  wget https://packages.chef.io/files/stable/chefdk/2.3.4/el/7/chefdk-2.3.4-1.el7.x86_64.rpm -P /tmp
+  rpm -Uvh /tmp/chefdk*.rpm
+fi
 
 cat >> /etc/hosts <<EOL
 192.168.123.10 chef-server
@@ -16,3 +17,9 @@ cat >> /etc/hosts <<EOL
 192.168.123.13 node3
 192.168.123.14 workstation
 EOL
+
+if [[ $(systemctl get-default) != 'graphical.target' ]]; then
+  yum -y group install "Server With GUI"
+  systemctl set-default graphical.target
+  reboot
+fi
